ef24dd2dc8312676c1f81423add82120ac5c72fb
qi xu <qi.xu@autronicafire.no>
2022-10-04 10:25:52 +0200

    wip

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,13 @@
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld.vue';
 </script>
 
 <template>
   <div>
     <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
+      <img src="./assets/babylon.png" class="babylon_logo" alt="Babylon logo" />
     </a>
     <a href="https://vuejs.org/" target="_blank">
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
@@ -17,13 +17,18 @@ import HelloWorld from './components/HelloWorld.vue'
 </template>
 
 <style scoped>
+.babylon_logo {
+  height: 7em;
+  padding: 0.2em;
+}
+
 .logo {
-  height: 6em;
+  height: 5em;
   padding: 1.5em;
   will-change: filter;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+.babylon_logo:hover {
+  filter: drop-shadow(0 0 2em #ff6565aa);
 }
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
