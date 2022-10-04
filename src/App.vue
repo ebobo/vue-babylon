4fae64dce25c679295806360ca76f15bc6bcd669
qi xu <qi.xu@autronicafire.no>
2022-10-04 11:09:30 +0200

    wip

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,26 @@
 <template>
-  <div>
-    <a href="https://www.babylonjs.com/" target="_blank">
-      <img src="./assets/babylon.png" class="babylon_logo" alt="Babylon logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <Home msg="Babylon + Vue" />
+  <v-app>
+    <v-main>
+      <HelloWorld/>
+    </v-main>
+  </v-app>
 </template>
 
-<script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import Home from './components/Home.vue';
+<script lang="ts">
+import { defineComponent } from 'vue'
+import HelloWorld from './components/HelloWorld.vue'
+
+export default defineComponent({
+  name: 'App',
+
+  components: {
+    HelloWorld,
+  },
+
+  data () {
+    return {
+      //
+    }
+  },
+})
 </script>
-
-<style scoped>
-.babylon_logo {
-  height: 7em;
-  padding: 0.2em;
-}
-
-.logo {
-  height: 5em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.babylon_logo:hover {
-  filter: drop-shadow(0 0 2em #ff6565aa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,43 @@
+<template>
+  <v-container>
+    <v-row class="text-center">
+      <v-col cols="4">
+        <v-img :src="babylon_logo" class="my-3" contain height="110" />
+      </v-col>
+      <v-col cols="4">
+        <v-img :src="vue_logo" class="mt-3" contain height="80" />
+      </v-col>
+      <v-col cols="4">
+        <v-img :src="vuefify_logo" class="mt-3" contain height="70" />
+      </v-col>
+    </v-row>
+    <v-row class="text-center">
+      <v-col class="mb-4">
+        <h2 class="display-2 font-weight-bold mb-3">
+          BabylonJS + Vue + Vuetify
+        </h2>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+// Logo
+import vuefify_logo from '../assets/logo.svg';
+import vue_logo from '../assets/vue.svg';
+import babylon_logo from '../assets/babylon.png';
+
+export default defineComponent({
+  name: 'HelloWorld',
+
+  data() {
+    return {
+      vue_logo,
+      babylon_logo,
+      vuefify_logo,
+    };
+  },
+});
+</script>
